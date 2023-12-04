b903bc4fc19710f20a6cf9346d8d9e9d2f00f414
Jenn <bg201801625@wmsu.edu.ph>
2023-12-04 18:43:25 +0800

    added await and new promise to the mail

--- app/lib/mail.ts
+++ app/lib/mail.ts
@@ -28,6 +28,18 @@ export async function sendMail({
     return;
   }
 
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transport.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+            reject(error);
+        } else {
+            console.log("Server is ready to take our messages");
+            resolve(success);
+        }
+    });
+});
   try {
     const sendResult = await transport.sendMail({
       from: SMTP_EMAIL,
@@ -36,8 +48,24 @@ export async function sendMail({
       html: body,
     });
     console.log(sendResult);
+
+    await new Promise((resolve, reject) => {
+      // send mail
+      transport.sendMail(sendResult, (err, info) => {
+          if (err) {
+              console.error(err);
+              reject(err);
+          } else {
+              console.log(info);
+              resolve(info);
+          }
+      });
+  });
+
   } catch (error) {
     console.log(error);
   }
+
+
 }
 
