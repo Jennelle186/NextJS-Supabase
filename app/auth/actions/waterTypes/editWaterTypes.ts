c8faa1688568f10ca28d30663dd959e943f685a9
Jenn <bg201801625@wmsu.edu.ph>
2023-12-22 17:23:48 +0800

    changed the update instead of the upsert

--- app/auth/actions/waterTypes/editWaterTypes.ts
+++ app/auth/actions/waterTypes/editWaterTypes.ts
@@ -6,14 +6,18 @@ import { cookies } from "next/headers"
 
 export async function editWaterType(formData: FormData) {
   try {
+    console.log(formData, "formData")
     const name = formData.get('name')
     const price = formData.get('price')
-    // const id = Number(formData.get('id'))
     const idValue = formData.get('id');
     console.log('ID value:', idValue);
     const cookieStore = cookies()
     const supabase = createServerActionClient({ cookies: () => cookieStore })
-    await supabase.from('water_type').update({name: name, price: price}).eq('id', idValue)
+    const {data, error} = await supabase.from('water_type').update({name: name, price: price}).eq('id', idValue)
+    console.log(data, "data water")
+    console.log(error, "error water type")
+
+    // await supabase.from('water_type').upsert({name: name, price: price}).eq('id', idValue)
     revalidatePath('/waterTypes')
     return { message: `You have successfully edited ${name}` }
   } catch (e) {
